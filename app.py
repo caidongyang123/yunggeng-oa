5fa732b8455f718370adefc07b830ad66fa10cfb
caidongyang123 <15712456982@163.com>
2026-08-16 15:53:13 +0800

    fix: 模块导入即建表(create_db)，修复 gunicorn 线上无表崩溃

--- app.py
+++ app.py
@@ -173,8 +173,11 @@ def healthz():
     return 'ok', 200
 
 
-if __name__ == '__main__':
+with app.app_context():
     create_db()
+
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_DEBUG', '1') == '1'
     app.run(host='0.0.0.0', port=port, debug=debug)
